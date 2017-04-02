0f6da612092153bc20815a4df5b32d43d401131e
Mitchell Hamann <captain@voyager.local>
2017-04-01 19:30:36 -0500

    adding no windows support for requesttoken

--- functions/Request-TrelloAccessToken.ps1
+++ functions/Request-TrelloAccessToken.ps1
@@ -18,14 +18,17 @@ function Request-TrelloAccessToken
 	
 		[Parameter()]
 		[ValidateNotNullOrEmpty()]
-		[int]$AuthTimeout = 30
+		[int]$AuthTimeout = 30, 
+
+		[Parameter()]
+		[Switch]$NoWindows
 		
 	)
-	
-	$ErrorActionPreference = 'Stop'
-	try
-	{
+	begin { 
 
+	}
+	process { 
+		$ErrorActionPreference = 'Stop'
 		If (!$ApiKey) { $ApiKey = $global:trelloConfig.ApiKey}
 
 		$httpParams = @{
@@ -34,43 +37,56 @@ function Request-TrelloAccessToken
 			'scope' = $Scope
 			'response_type' = 'token'
 			'name' = $ApplicationName
-			'return_url' = 'https://trello.com'			
+			'return_url' = 'https://trello.com' # do not need this when -NoWindows is present 		
 		}
 		
 		$keyValues = @()
 		$baseUrl = 'https://trello.com/1/authorize/?'
 		$httpParams.GetEnumerator() | sort-object Name | foreach{
-			$keyValues += "$($_.Key)=$($_.Value)"
+			if($NoWindows -and $_.key -eq 'return_url') { 
+				return 
+			} else { 
+				$keyValues += "$($_.Key)=$($_.Value)"
+			}
 		}
 		
 		$keyValueString = $keyValues -join '&'
-		$authUri = "$baseUrl$keyValueString"
-
-		$IE = New-Object -ComObject InternetExplorer.Application
-		$null = $IE.Navigate($authUri)
-		$null = $IE.Visible = $true
-		
-		$timer = [System.Diagnostics.Stopwatch]::StartNew()
-		while (($IE.LocationUrl -notmatch '^https://trello.com/token=') -and ($timer.Elapsed.TotalSeconds -lt $AuthTimeout))
-		{
-			Start-Sleep -Seconds 1
+		$authUri = "$baseUrl/$keyValueString"
+		if (!$NoWindows){ 
+			try {
+				$IE = New-Object -ComObject InternetExplorer.Application
+				$null = $IE.Navigate($authUri)
+				$null = $IE.Visible = $true
+				
+				$timer = [System.Diagnostics.Stopwatch]::StartNew()
+				while (($IE.LocationUrl -notmatch '^https://trello.com/token=') -and ($timer.Elapsed.TotalSeconds -lt $AuthTimeout))
+				{
+					Start-Sleep -Seconds 1
+				}
+				$timer.Stop()
+				
+				if ($timer.Elapsed.TotalSeconds -ge $AuthTimeout)
+				{
+					throw 'Timeout waiting for user authorization.'
+				}
+				
+				$token = [regex]::Match($IE.LocationURL, 'token=(.+)').Groups[1].Value
+			}
+			catch
+			{
+				Write-Error $_.Exception.Message
+			}
+			finally
+			{
+				$null = $IE.Quit()	
+			}
+		} else { 
+			open $authUri
+			$token = Read-Host -Prompt 'Please authenticate PowerTrello and enter the token returned...'
 		}
-		$timer.Stop()
-		
-		if ($timer.Elapsed.TotalSeconds -ge $AuthTimeout)
-		{
-			throw 'Timeout waiting for user authorization.'
-		}
-		
-		[regex]::Match($IE.LocationURL, 'token=(.+)').Groups[1].Value
-		
 	}
-	catch
-	{
-		Write-Error $_.Exception.Message
-	}
-	finally
-	{
-		$null = $IE.Quit()	
+	
+	end { 
+		return $token
 	}
 }
