f45d808b7cc07556836cbd870d43eaf9b1ebef9e
Mitch Hamann <mhamann@shiplps.local>
2017-04-14 15:28:31 -0500

    removed clutter from Get-TrelloCard

--- functions/Get-TrelloCard.ps1
+++ functions/Get-TrelloCard.ps1
@@ -29,65 +29,16 @@ function Get-TrelloCard {
         $uri = "$baseUrl/boards/{0}/cards?filter=all&$string"
         $cards = @() 	
 
-        # function Get-Cards { 
-        #     [CmdletBinding()]
-        #     Param 
-        #     ( 
-        #         [Parameter()]
-        #         [String]$id, 
-		# 		[string]$RequestUri
-        #     )
-			
-        #     $results = @()
-        #     $cardCall = Invoke-RestMethod -Uri $RequestUri
-
-        #     # Get the created date of the card since this is pulled from the id. 
-        #     foreach ( $c in $cardCall){ 
-        #         
-        #     }
-		# 	return $results 
-        # }
     }
     process {
 
-            $request = $uri -f $id
-            $return = Invoke-RestMethod -uri $request
+        $request = $uri -f $id
+        $return = Invoke-RestMethod -uri $request
 
-            foreach ($card in $return ){ 
-                $createdDate = Convert-IdToDate -ObjectId $card.Id
-                Add-Member -InputObject $card -NotePropertyName CreatedDate -NotePropertyValue $createdDate 
-                $cards += $card
-            }
-            
-        # try {
-		# 	# foreach ($board in $boards) {               
-        #         # # have not tested all of these just yet. 
-        #         # if ($PSBoundParameters.ContainsKey('Label')) {                    
-        #         #      $cards += $boardCards| Where-Object { if (($_.labels) -and $_.labels.Name -contains $Label) { $true } }
-        #         # }
-        #         # elseif ($PSBoundParameters.ContainsKey('Due'))
-        #         # {
-        #         #     $cards = $boardCards
-        #         # }
-        #         # elseif ($PSBoundParameters.ContainsKey('Name'))
-        #         # {
-        #         #     $cards = $boardCards | Where-Object {$_.Name -eq $Name}
-        #         # }
-        #         # elseif ($PSBoundParameters.ContainsKey('Id'))
-        #         # {
-        #         #     $cards = $boardCards| Where-Object {$_.idShort -eq $Id}
-        #         # }
-        #         # else
-        #         # {
-        #         #     $cards = $boardCards
-        #         # }
-        #     }
-        # }
-        # catch {
-        #     Write-Error $_.Exception.Message
-        # }
+        foreach ($card in $return ){ 
+            $createdDate = Convert-IdToDate -ObjectId $card.Id
+            Add-Member -InputObject $card -NotePropertyName CreatedDate -NotePropertyValue $createdDate 
+            $cards += $card
+        }
     }
-	end { 
-		return $cards
-	}
 }
